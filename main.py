5e75fc58dbfe281b159b0693cb66eb25d6f621c8
HYEONGYEOMWOO <108976686+eosl7808@users.noreply.github.com>
2023-09-08 20:19:33 +0900

    CRUD 메모장
    DB없는 간단한 CRUD 메모장

---
+++ main.py
@@ -0,0 +1,45 @@
+
+
+from fastapi import FastAPI
+
+from pydantic import BaseModel
+
+from fastapi.staticfiles import StaticFiles
+
+class Memo(BaseModel):
+    id: int
+    content:str
+    
+memos=[]
+
+app = FastAPI()
+
+@app.post("/memos")
+def create_memo(memo:Memo):
+    memos.append(memo)
+    return 'success'   
+
+@app.get("/memos")
+def read_memo():
+    return memos
+
+
+@app.put("/memos/{memo_id}")
+def put_memo(req_memo:Memo):
+    for memo in memos:
+        if memo.id== req_memo.id:
+            memo.content= req_memo.content
+            return '성공했습니다.'
+    return '그런 메모는 없어요.'
+
+@app.delete("/memos/{memo_id}")
+def delete_memo(memo_id:int):
+    for index, memo in enumerate (memos):
+        if memo.id== memo_id:
+            memos.pop(index)
+            return '성공했습니다.'
+    return '그런 메모는 없어요.'
+
+app.mount("/", StaticFiles(directory="static",html=True), name="static")
+
+
